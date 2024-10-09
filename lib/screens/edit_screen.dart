2e00fbe2e7c64ab5ffe09008d4c9e0cc3b785f06
Takt <thanapols65@nu.ac.th>
2024-10-09 15:28:08 +0700

    Account

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,12 +1,11 @@
 import 'package:account/main.dart';
 import 'package:account/models/transactions.dart';
 import 'package:account/provider/transaction_provider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class EditScreen extends StatefulWidget {
-  Transactions statement;
+  final Transactions statement;
 
   EditScreen({super.key, required this.statement});
 
@@ -18,86 +17,135 @@ class _EditScreenState extends State<EditScreen> {
   final formKey = GlobalKey<FormState>();
 
   final titleController = TextEditingController();
-
   final companyController = TextEditingController();
-
   final styleController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     titleController.text = widget.statement.title;
     companyController.text = widget.statement.company;
     styleController.text = widget.statement.style;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('ฟอร์มแก้ไขข้อมูล'),
+      appBar: AppBar(
+        title: const Text('ฟอร์มแก้ไขข้อมูล'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'ชื่อ Vtuber',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide:
+                        const BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(10),
+                ),
+                autofocus: false,
+                controller: titleController,
+                validator: (String? str) {
+                  if (str == null || str.isEmpty) {
+                    return 'กรุณากรอกข้อมูล';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'สังกัดค่าย',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide:
+                        const BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(10),
+                ),
+                autofocus: false,
+                controller: companyController,
+                validator: (String? str) {
+                  if (str == null || str.isEmpty) {
+                    return 'กรุณากรอกข้อมูล';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'แนวการสตรีม',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide:
+                        const BorderSide(color: Colors.blue, width: 2.0),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.all(10),
+                ),
+                autofocus: false,
+                controller: styleController,
+                validator: (String? str) {
+                  if (str == null || str.isEmpty) {
+                    return 'กรุณากรอกข้อมูล';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 12.0, horizontal: 20.0),
+                ),
+                child: const Text(
+                  'แก้ไขข้อมูล',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    var statement = Transactions(
+                      keyID: widget.statement.keyID,
+                      title: titleController.text,
+                      company: companyController.text,
+                      style: styleController.text,
+                      date: DateTime.now(),
+                    );
+
+                    var provider = Provider.of<TransactionProvider>(context,
+                        listen: false);
+                    provider.updateTransaction(statement);
+
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        fullscreenDialog: true,
+                        builder: (context) {
+                          return MyHomePage();
+                        },
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
-        body: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'ชื่อVtuber',
-                  ),
-                  autofocus: false,
-                  controller: titleController,
-                  validator: (String? str) {
-                    if (str!.isEmpty) {
-                      return 'กรุณากรอกข้อมูล';
-                    }
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'สังกัดค่าย',
-                  ),
-                  autofocus: false,
-                  controller: companyController,
-                  validator: (String? str) {
-                    if (str!.isEmpty) {
-                      return 'กรุณากรอกข้อมูล';
-                    }
-                  },
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'แนวการสตรีม',
-                  ),
-                  autofocus: false,
-                  controller: styleController,
-                  validator: (String? str) {
-                    if (str!.isEmpty) {
-                      return 'กรุณากรอกข้อมูล';
-                    }
-                  },
-                ),
-                TextButton(
-                    child: const Text('แก้ไขข้อมูล'),
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        var statement = Transactions(
-                            keyID: widget.statement.keyID,
-                            title: titleController.text,
-                            company: companyController.text,
-                            style: styleController.text,
-                            date: DateTime.now());
-
-                        var provider = Provider.of<TransactionProvider>(context,
-                            listen: false);
-
-                        provider.updateTransaction(statement);
-
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                fullscreenDialog: true,
-                                builder: (context) {
-                                  return MyHomePage();
-                                }));
-                      }
-                    })
-              ],
-            )));
+      ),
+    );
   }
 }
